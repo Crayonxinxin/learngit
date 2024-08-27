02d42a7a86ec2060dc585bc756d0f71f0e16d1b4
caryonxiaoxin <2941451402@qq.com>
2024-08-27 11:24:11 +0800

    a new file

---
+++ test.cpp
@@ -0,0 +1,27 @@
+/*
+ * @Description: 
+ * @Author: CrayonXiaoxin
+ * @Date: 2024-08-27 11:20:15
+ * @LastEditTime: 2024-08-27 11:20:24
+ * @LastEditors:  
+ */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+const int maxn = 1e5 + 10;
+
+void solve(){
+    
+}
+
+int main(){
+//std::ios::sync_with_stdio(false);
+//cin.tie(0);
+//cout.tie(0);
+int t=1;
+//cin>>t;
+while(t--){
+    solve();
+}
+return 0;
+}
