c52d6e7db06fa36fd55015693fe90abd2e5ac567
caryonxiaoxin <2941451402@qq.com>
2024-08-27 16:32:30 +0800

    delete test.cpp

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+const int maxn = 1e5 + 10;
+
+void solve(){
+    
+}
+
+int main(){
+//std::ios::sync_with_stdio(false);
+//cin.tie(0);
+//cout.tie(0);
+int t=1;
+//cin>>t;
+while(t--){
+    solve();
+}
+return 0;
+}
